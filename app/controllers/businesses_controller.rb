d20777af6a15242a32808b2c6700ea362282e866
Ashok Kumar <k.ashok502@gmail.com>
2013-07-19 18:17:53 +0530

    small changes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -15,21 +15,21 @@ class BusinessesController < ApplicationController
   end
 
   def cities
-    @cities = Business.select('DISTINCT city,state').where("city LIKE '#{params[:city]}%'")
+    @cities = Business.select('DISTINCT city,state').where("city ILIKE '#{params[:city]}%'")
     respond_to do |format|
       format.js
     end
   end
 
   def city_businesses
-    @cities = Business.select('DISTINCT name').where("name LIKE '#{params[:name].split("'").first}%' and city = '#{session[:city]}'")
+    @cities = Business.select('DISTINCT name').where("name ILIKE '#{params[:name].split("'").first}%' and city = '#{session[:city]}'")
     respond_to do |format|
       format.js
     end
   end
 
   def search
-    @ab_business_databases = Business.where("name LIKE '#{params[:business][:name].split("'").first}%'").limit(1)
+    @ab_business_databases = Business.where("name ILIKE '#{params[:business][:name].split("'").first}%'").limit(1)
 
     for ccategory in @ab_business_databases
       @categories = Business.where("name = '#{ccategory.name.split("'").first}}' and id NOT IN (#{@ab_business_databases.first.id})").paginate :page => params[:category_page], :per_page => 9
