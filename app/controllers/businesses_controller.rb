24356225c8a4df4e825f379332bdae7c038d885d
Ashok Kumar <k.ashok502@gmail.com>
2013-07-24 16:51:10 +0530

    logo only clickable

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -6,7 +6,7 @@ class BusinessesController < ApplicationController
       session[:city] = params[:city]
       session[:state] = params[:state]
     end
-    puts session[:state]
+    session[:state]
     if request.xhr?
       respond_to do |format|
         format.js
@@ -15,27 +15,27 @@ class BusinessesController < ApplicationController
   end
 
   def cities
-    @cities = Business.select('DISTINCT city,state').where("city LIKE '#{params[:city]}%'")
+    @cities = Business.select('DISTINCT city,state').where("city ILIKE '#{params[:city]}%'").limit(10)
     respond_to do |format|
       format.js
     end
   end
 
   def city_businesses
-    @cities = Business.select('DISTINCT name').where("name LIKE '#{params[:name].split("'").first}%' and city = '#{session[:city]}'")
+    @cities = Business.select('DISTINCT name').where("name ILIKE '#{params[:name].split("'").first}%' and city = '#{session[:city]}'")
     respond_to do |format|
       format.js
     end
   end
 
   def search
-    @ab_business_databases = Business.where("name LIKE '#{params[:business][:name].split("'").first}%'").limit(1)
+    @ab_business_databases = Business.where("name ILIKE '#{params[:business][:name].split("'").first}%'").limit(1)
     if @ab_business_databases.empty?
       @spelling_suggestion = Business.search_spelling_suggestions(params[:business][:name].split("'").first)
     end
 
     for ccategory in @ab_business_databases
-      @categories = Business.where("category = '#{ccategory.category.split("'").first}' and city IS NOT NULL and id NOT IN (#{@ab_business_databases.first.id})").paginate :page => params[:category_page], :per_page => 9
+      @categories = Business.where("category = '#{ccategory.category.split("'").first}' and address IS NOT NULL and city IS NOT NULL and id NOT IN (#{@ab_business_databases.first.id})").paginate :page => params[:category_page], :per_page => 9
     end
   end
 end
