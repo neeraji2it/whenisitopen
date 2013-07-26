078838fb2a0edb798c21ad4942f299eae66d515e
Ashok Kumar <k.ashok502@gmail.com>
2013-07-26 13:15:22 +0530

    only show the open businesses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,19 @@ module ApplicationHelper
   def datee(ccategory)
     @date = Date.today.strftime("%a").downcase
     if @date == 'mon'
-      ccategory.mon_to.split(":").first.to_i
+      ccategory.mon_to.to_i
     elsif @date == 'tue'
-      ccategory.tue_to.split(":").first.to_i
+      ccategory.tue_to.to_i
     elsif @date == 'wed'
-      ccategory.wed_to.split(":").first.to_i
+      ccategory.wed_to.to_i
     elsif @date == 'thu'
-      ccategory.thu_to.split(":").first.to_i
+      ccategory.thu_to.to_i
     elsif @date == 'fri'
-      ccategory.fri_to.split(":").first.to_i
+      ccategory.fri_to.to_i
     elsif @date == 'sat'
-      ccategory.sat_to.split(":").first.to_i
+      ccategory.sat_to.to_i
     else
-      ccategory.sun_to.split(":").first.to_i
+      ccategory.sun_to.to_i
     end
   end
 end
