83b263ba5b0c61534281f7650dc939944db184fa
rails <rails@my-desktop.(none)>
2013-07-26 18:09:53 +0530

    small changes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -33,10 +33,7 @@ class BusinessesController < ApplicationController
     if @ab_business_databases.empty?
       @spelling_suggestion = Business.search_spelling_suggestions(params[:business][:name].split("'").first)
     end
-
-    for ccategory in @ab_business_databases
-      a = datee(ccategory)
-      @categories = Business.where("category = '#{ccategory.category.split("'").first}' and #{a} > #{Time.now.strftime("%H").to_i - 12} and address IS NOT NULL and city IS NOT NULL and id NOT IN (#{@ab_business_databases.first.id})").paginate :page => params[:category_page], :per_page => 9
-    end
+    a = Date.today.strftime("%a").downcase+"_to"
+    @categories = Business.where("category = '#{@ab_business_databases.first.category.split("'").first}' and #{a} > #{Time.now.strftime("%H").to_i - 12} and address IS NOT NULL and city IS NOT NULL and id NOT IN (#{@ab_business_databases.first.id})").paginate :page => params[:category_page], :per_page => 9
   end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,5 @@
 class Business < ActiveRecord::Base
-  attr_accessible :name,:url,:address,:city,:state,:zip_code,:phone,:longitude,:latitude,:category,:mon_from,:mon_to,:tue_from,:tue_to,:wed_from,:wed_to,:thur_from,:thur_to,:fri_from,:fri_to,:sat_from,:sat_to,:sun_from,:sun_to
+  attr_accessible :name,:url,:address,:city,:state,:zip_code,:phone,:longitude,:latitude,:category,:mon_from,:mon_to,:tue_from,:tue_to,:wed_from,:wed_to,:thu_from,:thu_to,:fri_from,:fri_to,:sat_from,:sat_to,:sun_from,:sun_to
 
   #  define_index do
   #    indexes name
