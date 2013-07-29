b6346543bbe18b578185a1a3736f2bc3a6d3a4bb
Ashok Kumar <k.ashok502@gmail.com>
2013-07-29 19:48:05 +0530

    small changes

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,10 +2,9 @@ class Business < ActiveRecord::Base
   attr_accessible :address,:city,:contact_name,:company_name,:fax_number,:gender, :sales,:major_division_description, :sic_4_code,:sic_2_code_description, :employee, :title, :url, :phone, :state, :zip_code,:longitude,:latitude, :category,:gmaps, :mon_from, :mon_to, :tue_from, :tue_to, :wed_from, :wed_to, :thu_from, :thu_to, :fri_from, :fri_to, :sat_from, :sat_to, :sun_from, :sun_to 
 
   #  define_index do
-  #    indexes name
-  #    indexes city
+  #    indexes comapny_name
   #  end
-
+  
   def self.search_spelling_suggestions(query)
     #word = find_by_sql(["SELECT word, similarity(word, :term) AS sim FROM words WHERE word % :term and word <> :term order by sim desc limit 1",{:term=>term}]).first
     #      word = find_by_sql(["SELECT word, levenshtein_ratio(word, :term) AS sim FROM words where word != :term order by sim desc limit 1", {:term =>term}]).first
