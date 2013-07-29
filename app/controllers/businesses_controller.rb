3c02038b533ad938ab985e227b592cdaba61593b
Ashok Kumar <k.ashok502@gmail.com>
2013-07-29 14:24:15 +0530

    small changes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -15,21 +15,21 @@ class BusinessesController < ApplicationController
   end
 
   def cities
-    @cities = Business.select('DISTINCT city,state').where("city ILIKE '#{params[:city]}%'")
+    @cities = Business.select('DISTINCT city,state').where("city like '#{params[:city]}%'")
     respond_to do |format|
       format.js
     end
   end
 
   def city_businesses
-    @cities = Business.select('DISTINCT name').where("name ILIKE '#{params[:name].split("'").first}%' and city = '#{session[:city]}'")
+    @cities = Business.select('DISTINCT name').where("name like '#{params[:name].split("'").first}%' and city = '#{session[:city]}'")
     respond_to do |format|
       format.js
     end
   end
 
   def search
-    @ab_business_databases = Business.where("name ILIKE '#{params[:business][:name].split("'").first}%'").limit(1)
+    @ab_business_databases = Business.where("name like '#{params[:business][:name].split("'").first}%'").limit(1)
     if @ab_business_databases.empty?
       @spelling_suggestion = Business.search_spelling_suggestions(params[:business][:name].split("'").first)
     end
