958a92610b480efbfffb70d0d75d366afe1e9c8c
Ashok Kumar <k.ashok502@gmail.com>
2013-09-05 19:12:26 +0530

    small changes

---
+++ db/migrate/20130905121629_add_timestamps_to_business.rb
@@ -0,0 +1,6 @@
+class AddTimestampsToBusiness < ActiveRecord::Migration
+  def change
+    add_column :businesses, :created_at, :datetime
+    add_column :businesses, :updated_at, :datetime
+  end
+end
