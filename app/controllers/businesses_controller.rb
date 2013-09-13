2c5e4ab29c8970b3b715cef0289974d05ed92762
Ashok Kumar <k.ashok502@gmail.com>
2013-09-13 18:26:24 +0530

    small changes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -47,12 +47,12 @@ class BusinessesController < ApplicationController
         @spelling_suggestion = Business.search_spelling_suggestions(@name, session[:city])
       else
         a = Date.today.strftime("%a").downcase+"_to"
-        @nears = Business.near(@name,100, :order =>:distance)
+        @nears = Business.near(@name,10000, :order =>:distance)
         @all_categories = Business.where("category = ? and #{a} > #{Time.zone.now.strftime("%H").to_i - 12} and address IS NOT NULL and city IS NOT NULL and address != ? and id NOT IN (?)", "#{@ab_business_databases.first.category}","#{@ab_business_databases.first.address}","#{@ab_business_databases.first.id}")
         @categorie_with_cond = @nears.where_values.reduce(:and)
         @categorie_with = @all_categories.where_values.reduce(:and)
         @categories = Business.where("(#{@categorie_with_cond}) and (#{@categorie_with})").paginate :page => params[:category_page], :per_page => 9
-        @all_locations = Business.where("company_name = ? and address IS NOT NULL and city IS NOT NULL and address != ? and id NOT IN (?) and (mon_from IS NOT NULL and tue_from IS NOT NULL and wed_from IS NOT NULL and thu_from IS NOT NULL and fri_from IS NOT NULL and sat_from IS NOT NULL and sun_from IS NOT NULL)","#{@ab_business_databases.first.company_name}","#{@ab_business_databases.first.address}","#{@ab_business_databases.first.id}")
+        @all_locations = Business.where("company_name = ? and address IS NOT NULL and city IS NOT NULL and address != ? and id NOT IN (?)","#{@ab_business_databases.first.company_name}","#{@ab_business_databases.first.address}","#{@ab_business_databases.first.id}")
         @locations_with_cond = @nears.where_values.reduce(:and)
         @locations_with = @all_locations.where_values.reduce(:and)
         @locations = Business.where("(#{@locations_with_cond}) and (#{@locations_with})").paginate :page => params[:location_page], :per_page => 3
@@ -65,12 +65,12 @@ class BusinessesController < ApplicationController
   def categorie_search
     @ab_business_databases = Business.search "(*#{params[:company_name]}*, *#{params[:city]}*, *#{params[:address]}*)", :limit => 1 
     a = Date.today.strftime("%a").downcase+"_to"
-    @nears = Business.near(params[:company_name],100, :order =>:distance)
+    @nears = Business.near(params[:company_name],10000, :order =>:distance)
     @all_categories = Business.where("category = ? and #{a} > #{Time.zone.now.strftime("%H").to_i - 12} and address IS NOT NULL and city IS NOT NULL and address != ? and id NOT IN (?)", "#{@ab_business_databases.first.category}","#{@ab_business_databases.first.address}","#{@ab_business_databases.first.id}")
     @categorie_with_cond = @nears.where_values.reduce(:and)
     @categorie_with = @all_categories.where_values.reduce(:and)
     @categories = Business.where("(#{@categorie_with_cond}) and (#{@categorie_with})").paginate :page => params[:category_page], :per_page => 9
-    @all_locations = Business.where("company_name = ? and address IS NOT NULL and city IS NOT NULL and address != ? and id NOT IN (?) and (mon_from IS NOT NULL and tue_from IS NOT NULL and wed_from IS NOT NULL and thu_from IS NOT NULL and fri_from IS NOT NULL and sat_from IS NOT NULL and sun_from IS NOT NULL)", "#{@ab_business_databases.first.company_name}","#{@ab_business_databases.first.address}","#{@ab_business_databases.first.id}")
+    @all_locations = Business.where("company_name = ? and address IS NOT NULL and city IS NOT NULL and address != ? and id NOT IN (?)","#{@ab_business_databases.first.company_name}","#{@ab_business_databases.first.address}","#{@ab_business_databases.first.id}")
     @locations_with_cond = @nears.where_values.reduce(:and)
     @locations_with = @all_locations.where_values.reduce(:and)
     @locations = Business.where("(#{@locations_with_cond}) and (#{@locations_with})").paginate :page => params[:location_page], :per_page => 3
