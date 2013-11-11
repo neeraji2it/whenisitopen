7bc2835fc1f684ec88a514a4d526f204d6a9abfe
Ashok Kumar <k.ashok502@gmail.com>
2013-11-11 16:33:19 +0530

    small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout :get_layout
   helper_method :after_sign_in_path_for
-  before_filter :search_limit?, :set_timezone
+  before_filter :set_timezone
 
 
   def after_sign_in_path_for(resource_or_scope)
@@ -26,16 +26,6 @@ class ApplicationController < ActionController::Base
   def login?
     unless current_admin
       redirect_to '/'
-    end
-  end
-  
-  def search_limit?
-    if SearchLimit.first.searching_limit < 2000
-      if (params[:controller] == 'businesses' and !(params[:location_page] or params[:category_page]) and (params[:action] == 'search' or params[:action] == 'categorie_search'))
-        SearchLimit.first.update_attribute(:searching_limit, SearchLimit.first.searching_limit+1)
-      end
-    else
-      render :partial => "/businesses/form1"
     end
   end
   
